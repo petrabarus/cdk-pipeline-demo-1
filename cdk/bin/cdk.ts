5f4ce01e0687e7e520883b6d9f845362c48460b4
Petra <ubuntu@ip-172-31-22-21.ap-southeast-1.compute.internal>
2021-08-12 08:06:59 +0000

    test

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { MyAppStack } from '../lib/myapp-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
-new MyAppStack(app, 'CdkStack');
+new PipelineStack(app, 'CdkPipelineStack');
+app.synth();
--- cdk/lib/pipeline-stack.ts
+++ cdk/lib/pipeline-stack.ts
@@ -1,2 +1,58 @@
 import * as cdk from '@aws-cdk/core';
+import * as cp from '@aws-cdk/aws-codepipeline';
+import * as cpa from '@aws-cdk/aws-codepipeline-actions';
 import * as pipelines from '@aws-cdk/pipelines';
+import { MyAppStack } from './myapp-stack';
+
+class MyAppStage extends cdk.Stage {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+        
+        const service = new MyAppStack(this, 'MyApp', {
+          tags: {
+            Application: 'MyApp',
+            Environment: id
+          }
+        });
+
+    }
+}
+
+export class PipelineStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+        
+        
+        const sourceArtifact = new cp.Artifact();
+        const cloudAssemblyArtifact = new cp.Artifact();
+        
+        const sourceAction = new cpa.GitHubSourceAction({
+            actionName: 'GitHub',
+            output: sourceArtifact,
+            oauthToken: cdk.SecretValue.secretsManager('GitHubAccess'),
+            owner: 'petrabarus',
+            repo: 'cdk-pipeline-demo-1',
+        });
+        
+        
+        const synthAction = pipelines.SimpleSynthAction.standardNpmSynth({
+            sourceArtifact,
+            cloudAssemblyArtifact,
+            
+            buildCommand: 'cd cdk && npm run build && npm test',
+        });
+        
+        
+        const pipeline = new pipelines.CdkPipeline(this, 'Pipeline', {
+            cloudAssemblyArtifact,
+            sourceAction,
+            synthAction
+        });
+        
+        const preProdApp = new MyAppStage(this, 'Pre-Prod');
+        const preProdStage = pipeline.addApplicationStage(preProdApp);
+        
+        const prodApp = new MyAppStage(this, 'Prod');
+        const prodStage = pipeline.addApplicationStage(prodApp);
+    }
+}
